77697f976b80f3610e65b8b26a71a5720cbd0120
chrisklus <christopher.klusendorf@colorado.edu>
2019-10-15 22:21:17 -0400

    Adding/removing dogs updates the current number, dogs animate when returning to bucket, see https://github.com/phetsims/number-play/issues/2

--- js/common/model/NumberPlayPlayArea.js
+++ js/common/model/NumberPlayPlayArea.js
@@ -9,15 +9,20 @@ define( require => {
   'use strict';
 
   // modules
+  const Animation = require( 'TWIXT/Animation' );
   const Bucket = require( 'PHETCOMMON/model/Bucket' );
   const Dimension2 = require( 'DOT/Dimension2' );
+  const Easing = require( 'TWIXT/Easing' );
   const merge = require( 'PHET_CORE/merge' );
   const numberPlay = require( 'NUMBER_PLAY/numberPlay' );
+  const ObservableArray = require( 'AXON/ObservableArray' );
   const PlayObject = require( 'NUMBER_PLAY/common/model/PlayObject' );
   const Vector2 = require( 'DOT/Vector2' );
 
   // constants
   const BUCKET_SIZE = new Dimension2( 100, 50 );
+  const ANIMATION_SPEED = 200; // in screen coordinates per second
+  const MAX_ANIMATION_TIME = 1; // in seconds
 
   class NumberPlayPlayArea {
 
@@ -42,14 +47,36 @@ define( require => {
         const y = phet.joist.random.nextDouble() * BUCKET_SIZE.height / 2 + BUCKET_SIZE.height / 2;
         this.playObjects.push( new PlayObject( new Vector2( x, y ), new Dimension2( 40, 40 ) ) );
       } );
+
+      // @private - track play objects that are added to the play area
+      this.playObjectsInPlayArea = new ObservableArray();
+
+      // if a play object is added or removed from the play area, update the current number
+      this.playObjectsInPlayArea.lengthProperty.link( length => {
+        currentNumberProperty.value = length;
+      } );
     }
 
     returnPlayObjectToBucket( playObject ) {
-      console.log( 'returning to bucket' );
+      this.playObjectsInPlayArea.contains( playObject ) && this.playObjectsInPlayArea.remove( playObject );
+
+      // calculate the time needed to get to the destination
+      const animationDuration = Math.min(
+        playObject.positionProperty.value.distance( playObject.positionProperty.initialValue ) / ANIMATION_SPEED,
+        MAX_ANIMATION_TIME
+      );
+      const animationOptions = {
+        property: playObject.positionProperty,
+        to: playObject.positionProperty.initialValue,
+        duration: animationDuration,
+        easing: Easing.CUBIC_IN_OUT
+      };
+      const translateAnimation = new Animation( animationOptions );
+      translateAnimation.start();
     }
 
     addPlayObjectToPlayArea( playObject ) {
-      console.log( 'adding to play area' );
+      !this.playObjectsInPlayArea.contains( playObject ) && this.playObjectsInPlayArea.push( playObject );
     }
 
     /**
