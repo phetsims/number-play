dccbc6b3ef93901b529e484aae9a8551a229fa5d
samreid <reids@colorado.edu>
2020-02-27 12:08:01 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/number-play-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import numberPlay from './numberPlay.js';
+
+const numberPlayStrings = getStringModule( 'NUMBER_PLAY' );
+
+numberPlay.register( 'numberPlayStrings', numberPlayStrings );
+
+export default numberPlayStrings;
