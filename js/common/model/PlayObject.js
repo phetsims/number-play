ee78b060f70288f8156511aef77acac06ce79869
phet-dev <phetadmin@gmail.com>
2020-02-28 03:38:07 -0700

    update copyright dates from daily grunt work

--- js/common/model/PlayObject.js
+++ js/common/model/PlayObject.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Model class for a play object, which is any draggable item in the ObjectsAccordionBox
--- js/compare/view/CompareAccordionBox.js
+++ js/compare/view/CompareAccordionBox.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Class for the `Objects` accordion box on the 'Compare' screen, which mixes the functionality of ObjectsAccordionBox
