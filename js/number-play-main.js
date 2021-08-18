16d0e055d0f45f05fefdbb18776eaca76e975171
Jesse <jesse.greenberg@colorado.edu>
2021-08-18 14:44:36 -0400

    enable voicingManager specifically

--- js/number-play-main.js
+++ js/number-play-main.js
@@ -8,6 +8,7 @@
 
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import voicingManager from '../../scenery/js/accessibility/voicing/voicingManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import CompareScreen from './compare/CompareScreen.js';
 import LabScreen from './lab/LabScreen.js';
@@ -29,6 +30,12 @@ const simOptions = {
     thanks: ''
   }
 };
+
+// This sim makes a rare usage of Speech Synthesis without the voicing feature. The voicingManager makes requests
+// to the browser to speak content using Speech Synthesis, but is generally disabled until requested by the user.
+// Since this sim doesn't support other "Voicing" features (Toolbar, ReadingBlocks, levels of verbocity), there is
+// no UI to enable it and speech will always happen when the user presses certain buttons in this sim.
+voicingManager.enabledProperty.value = true;
 
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
