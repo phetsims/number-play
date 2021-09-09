8ef4a2516de15239567e4a89f540a5c464dc30a0
chrisklus <christopher.klusendorf@colorado.edu>
2021-09-08 23:55:51 -0600

    Switch word and ten frame accordion box, see https://github.com/phetsims/number-play/issues/49

--- js/common/view/NumberPlayScreenView.js
+++ js/common/view/NumberPlayScreenView.js
@@ -85,15 +85,15 @@ class NumberPlayScreenView extends ScreenView {
     this.onesAccordionBoxExpandedProperty = new BooleanProperty( true );
     this.objectsAccordionBoxExpandedProperty = new BooleanProperty( true );
 
-    // create and add the WordAccordionBox
-    const wordAccordionBox = new WordAccordionBox(
+    // create and add the TenFrameAccordionBox
+    const tenFrameAccordionBox = new TenFrameAccordionBox(
       model.currentNumberProperty,
       config.upperAccordionBoxHeight, merge( {
-        expandedProperty: this.wordAccordionBoxExpandedProperty
-      }, config.wordAccordionBoxConfig ) );
-    wordAccordionBox.left = this.layoutBounds.minX + NumberPlayConstants.ACCORDION_BOX_X_MARGIN;
-    wordAccordionBox.top = this.layoutBounds.minY + NumberPlayConstants.ACCORDION_BOX_TOP_MARGIN;
-    this.addChild( wordAccordionBox );
+        expandedProperty: this.tenFrameAccordionBoxExpandedProperty
+      }, config.tenFrameAccordionBoxConfig ) );
+    tenFrameAccordionBox.left = this.layoutBounds.minX + NumberPlayConstants.ACCORDION_BOX_X_MARGIN;
+    tenFrameAccordionBox.top = this.layoutBounds.minY + NumberPlayConstants.ACCORDION_BOX_TOP_MARGIN;
+    this.addChild( tenFrameAccordionBox );
 
     // create and add the NumeralAccordionBox
     const numeralAccordionBox = new NumeralAccordionBox(
@@ -102,18 +102,18 @@ class NumberPlayScreenView extends ScreenView {
         expandedProperty: this.numeralAccordionBoxExpandedProperty
       }, config.numeralAccordionBoxConfig ) );
     numeralAccordionBox.centerX = this.layoutBounds.centerX;
-    numeralAccordionBox.top = wordAccordionBox.top;
+    numeralAccordionBox.top = tenFrameAccordionBox.top;
     this.addChild( numeralAccordionBox );
 
-    // create and add the TenFrameAccordionBox
-    const tenFrameAccordionBox = new TenFrameAccordionBox(
+    // create and add the WordAccordionBox
+    const wordAccordionBox = new WordAccordionBox(
       model.currentNumberProperty,
       config.upperAccordionBoxHeight, merge( {
-        expandedProperty: this.tenFrameAccordionBoxExpandedProperty
-      }, config.tenFrameAccordionBoxConfig ) );
-    tenFrameAccordionBox.right = this.layoutBounds.maxX - NumberPlayConstants.ACCORDION_BOX_X_MARGIN;
-    tenFrameAccordionBox.top = wordAccordionBox.top;
-    this.addChild( tenFrameAccordionBox );
+        expandedProperty: this.wordAccordionBoxExpandedProperty
+      }, config.wordAccordionBoxConfig ) );
+    wordAccordionBox.right = this.layoutBounds.maxX - NumberPlayConstants.ACCORDION_BOX_X_MARGIN;
+    wordAccordionBox.top = numeralAccordionBox.top;
+    this.addChild( wordAccordionBox );
 
     // create and add the OnesAccordionBox
     const onesAccordionBox = new OnesAccordionBox(
