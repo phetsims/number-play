e6fe6e1131b91065ab9a37f7f5bf1f6c1c3707a2
chrisklus <christopher.klusendorf@colorado.edu>
2021-10-22 14:19:21 -0600

    Combine rotation and shape arrays and simplify model subitizing logic, see https://github.com/phetsims/number-play/issues/62

--- js/game/model/SubitizerModel.js
+++ js/game/model/SubitizerModel.js
@@ -3,47 +3,63 @@
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import dotRandom from '../../../../dot/js/dotRandom.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 import numberPlay from '../../numberPlay.js';
 
+// constants
+
+// angles, all in radians
+const DEGREES_45 = Math.PI * 0.25;
+const DEGREES_90 = Math.PI * 0.5;
+const DEGREES_135 = Math.PI * 0.75;
+const DEGREES_180 = Math.PI;
+const DEGREES_270 = Math.PI * ( 3 / 2 );
+
+// convenience function for easier reading
+const v2 = ( x, y ) => new Vector2( x, y );
+
 const SHAPES = {
-  1: [
-    [ { x: 0, y: 0 } ] // centered dot
-  ],
-  2: [
-    [ { x: -0.5, y: 0 }, { x: 0.5, y: 0 } ] // row
-  ],
-  3: [
-    [ { x: -1, y: 0 }, { x: 0, y: 0 }, { x: 1, y: 0 } ], // row
-    [ { x: -1, y: -1 }, { x: 0, y: 0 }, { x: 1, y: 1 } ], // diagonal row
-    [ { x: -1, y: 1 }, { x: 0, y: -1 }, { x: 1, y: 1 } ] // triangle pointing up
-  ],
-  4: [
-    [ { x: -1.5, y: 0 }, { x: -0.5, y: 0 }, { x: 0.5, y: 0 }, { x: 1.5, y: 0 } ], // row
-    [ { x: -1, y: -1 }, { x: 1, y: -1 }, { x: -1, y: 1 }, { x: 1, y: 1 } ] // square
-  ],
-  5: [
-    [ { x: -2, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 } ], // row
-    [ { x: -1, y: -1 }, { x: 1, y: -1 }, { x: 0, y: 0 }, { x: -1, y: 1 }, { x: 1, y: 1 } ], // 5 in a "die" formation
-    [ { x: 0, y: -1 }, { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: -0.5 }, { x: 1, y: 0.5 } ], // 3 left and 2 centered on right
-    [ { x: 0, y: -1 }, { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: -1 }, { x: 1, y: 0 } ], // 3 left and 2 top right
-    [ { x: 0, y: -1 }, { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 1 } ] // 3 left and 2 bottom right
-  ]
-};
-const ROTATION = {
-  2: [
-    [ 3.14 / 4, 3.14 / 2, ( 3 / 4 ) * 3.14 ] // rotated 45, 90, 135
-  ],
-  3: [
-    [ 3.14 / 2 ], // rotated 90
-    [ 3.14 / 2 ], // rotated 90
-    [ 3.14 / 2, 3.14, ( 3 / 2 ) * 3.14 ] // rotated 90, 180, 270
-  ],
-  4: [
-    [ 3.14 / 4 ] // rotated 45
-  ],
-  5: [
-    [ 3.14 / 2, 3.14, ( 3 / 2 ) * 3.14 ] // rotated 90, 180, 270
-  ]
+  1: [ {
+    coordinates: [ v2( 0, 0 ) ], // centered dot
+    rotations: []
+  } ],
+  2: [ {
+    coordinates: [ v2( -0.5, 0 ), v2( 0.5, 0 ) ], // row
+    rotations: [ DEGREES_45, DEGREES_90, DEGREES_135 ]
+  } ],
+  3: [ {
+    coordinates: [ v2( -1, 0 ), v2( 0, 0 ), v2( 1, 0 ) ], // row
+    rotations: [ DEGREES_90 ]
+  }, {
+    coordinates: [ v2( -1, -1 ), v2( 0, 0 ), v2( 1, 1 ) ], // diagonal row
+    rotations: [ DEGREES_90 ]
+  }, {
+    coordinates: [ v2( -1, 1 ), v2( 0, -1 ), v2( 1, 1 ) ], // triangle pointing up
+    rotations: [ DEGREES_90, DEGREES_180, DEGREES_270 ]
+  } ],
+  4: [ {
+    coordinates: [ v2( -1.5, 0 ), v2( -0.5, 0 ), v2( 0.5, 0 ), v2( 1.5, 0 ) ], // row
+    rotations: []
+  }, {
+    coordinates: [ v2( -1, -1 ), v2( 1, -1 ), v2( -1, 1 ), v2( 1, 1 ) ], // square
+    rotations: [ DEGREES_45 ]
+  } ],
+  5: [ {
+    coordinates: [ v2( -2, 0 ), v2( -1, 0 ), v2( 0, 0 ), v2( 1, 0 ), v2( 2, 0 ) ], // row
+    rotations: []
+  }, {
+    coordinates: [ v2( -1, -1 ), v2( 1, -1 ), v2( 0, 0 ), v2( -1, 1 ), v2( 1, 1 ) ], // 5 in a "die" formation
+    rotations: []
+  }, {
+    coordinates: [ v2( 0, -1 ), v2( 0, 0 ), v2( 0, 1 ), v2( 1, -0.5 ), v2( 1, 0.5 ) ], // 3 left and 2 centered on right
+    rotations: [ DEGREES_90, DEGREES_180, DEGREES_270 ]
+  }, {
+    coordinates: [ v2( 0, -1 ), v2( 0, 0 ), v2( 0, 1 ), v2( 1, -1 ), v2( 1, 0 ) ], // 3 left and 2 top right
+    rotations: [ DEGREES_90, DEGREES_180, DEGREES_270 ]
+  }, {
+    coordinates: [ v2( 0, -1 ), v2( 0, 0 ), v2( 0, 1 ), v2( 1, 0 ), v2( 1, 1 ) ], // 3 left and 2 bottom right
+    rotations: [ DEGREES_90, DEGREES_180, DEGREES_270 ]
+  } ]
 };
 
 /**
@@ -67,24 +83,18 @@ class SubitizerModel {
       this.rotationProperty.reset();
       let coordinates = [];
 
-      // whether the pattern uses an arranged configuration or a random one
-      const arranged = dotRandom.nextBoolean();
-      if ( arranged ) {
+      // 50/50 chance whether the pattern uses an arranged configuration or a random one
+      if ( dotRandom.nextBoolean() ) {
 
         // pick out a random shape for the corresponding subitizeNumber
-        let randomPatternIndex = dotRandom.nextInt( SHAPES[ subitizeNumber ].length );
-        coordinates = SHAPES[ subitizeNumber ][ randomPatternIndex ];
+        const randomPatternIndex = dotRandom.nextInt( SHAPES[ subitizeNumber ].length );
+        const shape = SHAPES[ subitizeNumber ][ randomPatternIndex ];
+        coordinates = shape.coordinates;
 
-        // if this shape is allowed to rotate, pick out a random corresponding rotation
-        if ( subitizeNumber !== 1 && !( subitizeNumber === 4 && randomPatternIndex === 0 ) &&
-             !( subitizeNumber === 5 && randomPatternIndex < 2 ) ) {
-
-          // subitizeNumber 4 and 5 only have one rotation option
-          if ( subitizeNumber === 5 || subitizeNumber === 4 ) {
-            randomPatternIndex = 0;
-          }
-          const randomRotationIndex = dotRandom.nextInt( ROTATION[ subitizeNumber ][ randomPatternIndex ].length );
-          this.rotationProperty.value = ROTATION[ subitizeNumber ][ randomPatternIndex ][ randomRotationIndex ];
+        // if the shape has rotations available, randomly pick one to assign
+        if ( shape.rotations.length ) {
+          const randomRotationIndex = dotRandom.nextInt( shape.rotations.length );
+          this.rotationProperty.value = shape.rotations[ randomRotationIndex ];
         }
       }
       else {
@@ -96,7 +106,7 @@ class SubitizerModel {
 
           // add a new coordinate if it doesn't exist yet
           if ( !_.find( coordinates, object => object.x === randomX && object.y === randomY ) ) {
-            coordinates.push( { x: randomX, y: randomY } );
+            coordinates.push( new Vector2( randomX, randomY ) );
           }
         }
       }
