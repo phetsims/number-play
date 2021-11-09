b82acf6a1bc3cf16217fb1a96ab149bb84d78396
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2021-11-09 15:57:53 -0700

    Made shape of the square with 4 objects smaller to fit the subitizer view in the game. See https://github.com/phetsims/number-play/issues/62.

--- js/game/view/NumberPlayGameLevelNode.ts
+++ js/game/view/NumberPlayGameLevelNode.ts
@@ -16,11 +16,27 @@ import Color from '../../../../scenery/js/util/Color.js';
 import InfiniteStatusBar from '../../../../vegas/js/InfiniteStatusBar.js';
 import numberPlay from '../../numberPlay.js';
 import SubitizeGameLevel from '../model/SubitizeGameLevel.js';
+import CardinalityCountGameLevel from '../model/CardinalityCountGameLevel.js';
+import FaceNode from '../../../../scenery-phet/js/FaceNode.js';
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import StarNode from '../../../../scenery-phet/js/StarNode.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
+import NumberPlayGameAnswerButtons from './NumberPlayGameAnswerButtons.js';
+import ArrowShape from '../../../../scenery-phet/js/ArrowShape.js';
+import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
+import Path from '../../../../scenery/js/nodes/Path.js';
+import NumberPlayQueryParameters from '../../common/NumberPlayQueryParameters.js';
+import Animation from '../../../../twixt/js/Animation.js';
+import Easing from '../../../../twixt/js/Easing.js';
 
 class NumberPlayGameLevelNode extends Node {
   public statusBar: InfiniteStatusBar;
   public level: SubitizeGameLevel;
   private layoutBounds: Bounds2;
+  private readonly frownyFaceNode: FaceNode;
+  private frownyFaceAnimation: Animation | null;
+  private readonly pointsAwardedNodeVisibleProperty: BooleanProperty;
+  private readonly answerButtons: NumberPlayGameAnswerButtons;
 
   /**
    * @param {NumberPlayGameLevel} level
@@ -63,6 +79,116 @@ class NumberPlayGameLevelNode extends Node {
     // @private {Bounds2}
     this.layoutBounds = layoutBounds;
 
+    // @private {FaceNode} - create and add frownyFaceNode which is visible when an incorrect answer button is pressed
+    this.frownyFaceNode = new FaceNode( 160 /* headDiameter */, {
+      visible: false
+    } );
+    this.frownyFaceNode.top = 144; // TODO magic number
+    this.frownyFaceNode.right = layoutBounds.maxX - 45; // empirically determined
+    this.frownyFaceNode.frown();
+    this.addChild( this.frownyFaceNode );
+    this.frownyFaceAnimation = null;
+
+    // create and add smileyFaceNode which is visible when a challenge is solved, meaning a correct answer button was pressed
+    const smileyFaceNode = new FaceNode( 160 /* headDiameter */, {
+      visibleProperty: level.isSolvedProperty
+    } );
+    smileyFaceNode.top = this.frownyFaceNode.top;
+    smileyFaceNode.centerX = this.frownyFaceNode.centerX;
+    this.addChild( smileyFaceNode );
+
+    // @private {BooleanProperty}
+    this.pointsAwardedNodeVisibleProperty = new BooleanProperty( false );
+
+    // create and add pointsAwardedNode which is shown when a correct guess is made on the first answerButtons press
+    const starNode = new StarNode( { value: 1, scale: 3 } );
+    starNode.centerX = this.frownyFaceNode.centerX;
+    starNode.top = this.frownyFaceNode.bottom + 40; // empirically determined
+    const pointsNode = new Text( '+1', { font: new PhetFont( 30 ), fill: 'black' } );
+    pointsNode.center = starNode.center;
+    pointsNode.centerY += 5;
+    const pointsAwardedNode = new Node( {
+      children: [ starNode, pointsNode ],
+      visibleProperty: this.pointsAwardedNodeVisibleProperty
+    } );
+    this.addChild( pointsAwardedNode );
+
+    // @private {NumberPlayGameAnswerButtons} - create and add the answerButtons
+    // @ts-ignore TODO-TS
+    this.answerButtons = new NumberPlayGameAnswerButtons( level,
+      this.pointsAwardedNodeVisibleProperty, showFrownyFace => this.setFrownyFaceVisibility( showFrownyFace ) );
+    this.answerButtons.centerX = layoutBounds.centerX;
+    this.answerButtons.bottom = layoutBounds.maxY - 58; // TODO magic number
+    this.addChild( this.answerButtons );
+
+    // create and add newChallengeButton which is visible when a challenge is solved, meaning a correct answer button was pressed
+    const arrowShape = new ArrowShape( 0, 0, 42, 0, {
+      tailWidth: 12,
+      headWidth: 25,
+      headHeight: 23
+    } );
+    const newChallengeButton = new RectangularPushButton( {
+      baseColor: Color.YELLOW,
+      xMargin: 27,
+      yMargin: 10.9,
+      content: new Path( arrowShape, { fill: 'black' } ),
+      visibleProperty: level.isSolvedProperty,
+      listener: () => this.newChallenge()
+    } );
+    newChallengeButton.centerX = smileyFaceNode.centerX;
+    newChallengeButton.centerY = this.answerButtons.centerY;
+    this.addChild( newChallengeButton );
+  }
+
+  public reset() {
+    this.pointsAwardedNodeVisibleProperty.reset();
+    this.answerButtons.reset();
+  }
+
+  /**
+   * Sets up a new challenge in the model and in the view.
+   */
+  protected newChallenge() {
+    // @ts-ignore TODO-TS
+    this.level.newChallenge();
+    this.pointsAwardedNodeVisibleProperty.value = false;
+    this.answerButtons.reset();
+    if ( NumberPlayQueryParameters.showCorrectAnswer ) {
+      // @ts-ignore TODO-TS
+      this.answerButtons.showCorrectAnswer( this.level.challengeNumberProperty );
+    }
+  }
+
+  /**
+   * Shows or hides a frowny face - if shown, animates it to fade out when the user made an incorrect guess.
+   *
+   * @param {boolean} showFrownyFace
+   */
+  private setFrownyFaceVisibility( showFrownyFace: boolean ) {
+
+    this.frownyFaceNode.visible = showFrownyFace;
+
+    if ( showFrownyFace ) {
+
+      // Animate opacity of frownyFaceNode, fade it out.
+      this.frownyFaceNode.opacityProperty.value = 1;
+      this.frownyFaceAnimation = new Animation( {
+        delay: 1,
+        duration: 0.8,
+        targets: [ {
+          property: this.frownyFaceNode.opacityProperty,
+          easing: Easing.LINEAR,
+          to: 0
+        } ]
+      } );
+
+      this.frownyFaceAnimation.finishEmitter.addListener( () => {
+        this.frownyFaceNode.visible = false;
+        this.frownyFaceAnimation = null;
+      } );
+
+      this.frownyFaceAnimation.start();
+    }
   }
 }
 
--- js/game/view/SubitizeGameLevelNode.ts
+++ js/game/view/SubitizeGameLevelNode.ts
@@ -7,16 +7,12 @@
  * @author Luisa Vargas
  */
 
-import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
-import ArrowShape from '../../../../scenery-phet/js/ArrowShape.js';
-import FaceNode from '../../../../scenery-phet/js/FaceNode.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import PlayIconShape from '../../../../scenery-phet/js/PlayIconShape.js';
 import ResetShape from '../../../../scenery-phet/js/ResetShape.js';
-import StarNode from '../../../../scenery-phet/js/StarNode.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
@@ -24,21 +20,17 @@ import Color from '../../../../scenery/js/util/Color.js';
 import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
 import Animation from '../../../../twixt/js/Animation.js';
 import Easing from '../../../../twixt/js/Easing.js';
-import NumberPlayQueryParameters from '../../common/NumberPlayQueryParameters.js';
 import SpeechSynthesisButton from '../../common/view/SpeechSynthesisButton.js';
 import numberPlay from '../../numberPlay.js';
 import SubitizeGameLevel from '../model/SubitizeGameLevel.js';
-import NumberPlayGameAnswerButtons from './NumberPlayGameAnswerButtons.js';
 import NumberPlayGameLevelNode from './NumberPlayGameLevelNode.js';
 import SubitizerNode from './SubitizerNode.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import numberPlayStrings from '../../numberPlayStrings.js';
+import CardinalityCountGameLevel from '../model/CardinalityCountGameLevel.js';
 
 class SubitizeGameLevelNode extends NumberPlayGameLevelNode {
-  private readonly frownyFaceNode: FaceNode;
-  private frownyFaceAnimation: Animation | null;
-  private readonly pointsAwardedNodeVisibleProperty: BooleanProperty;
-  private readonly answerButtons: NumberPlayGameAnswerButtons;
+
   private textObjectAnimation: Animation | null;
   private readonly startSequenceNode: Node;
 
@@ -112,71 +104,13 @@ class SubitizeGameLevelNode extends NumberPlayGameLevelNode {
     showAgainButton.setBottom( subitizerNode.getBottom() );
     this.addChild( showAgainButton );
 
-    // @private {FaceNode} - create and add frownyFaceNode which is visible when an incorrect answer button is pressed
-    this.frownyFaceNode = new FaceNode( 160 /* headDiameter */, {
-      visible: false
-    } );
-    this.frownyFaceNode.top = subitizerNode.top;
-    this.frownyFaceNode.right = layoutBounds.maxX - 45; // empirically determined
-    this.frownyFaceNode.frown();
-    this.addChild( this.frownyFaceNode );
-    this.frownyFaceAnimation = null;
-
-    // create and add smileyFaceNode which is visible when a challenge is solved, meaning a correct answer button was pressed
-    const smileyFaceNode = new FaceNode( 160 /* headDiameter */, {
-      visibleProperty: level.isSolvedProperty
-    } );
-    smileyFaceNode.top = subitizerNode.top;
-    smileyFaceNode.centerX = this.frownyFaceNode.centerX;
-    this.addChild( smileyFaceNode );
-
-    // @private {BooleanProperty}
-    this.pointsAwardedNodeVisibleProperty = new BooleanProperty( false );
-
-    // create and add pointsAwardedNode which is shown when a correct guess is made on the first answerButtons press
-    const starNode = new StarNode( { value: 1, scale: 3 } );
-    starNode.centerX = this.frownyFaceNode.centerX;
-    starNode.top = this.frownyFaceNode.bottom + 40; // empirically determined
-    const pointsNode = new Text( '+1', { font: new PhetFont( 30 ), fill: 'black' } );
-    pointsNode.center = starNode.center;
-    pointsNode.centerY += 5;
-    const pointsAwardedNode = new Node( {
-      children: [ starNode, pointsNode ],
-      visibleProperty: this.pointsAwardedNodeVisibleProperty
-    } );
-    this.addChild( pointsAwardedNode );
-
-    // @private {NumberPlayGameAnswerButtons} - create and add the answerButtons
-    this.answerButtons = new NumberPlayGameAnswerButtons( level,
-      this.pointsAwardedNodeVisibleProperty, showFrownyFace => this.setFrownyFaceVisibility( showFrownyFace ) );
-    this.answerButtons.centerX = subitizerNode.centerX;
-    this.answerButtons.top = subitizerNode.bottom + 40; // empirically determined
-    this.addChild( this.answerButtons );
-
-    // create and add newChallengeButton which is visible when a challenge is solved, meaning a correct answer button was pressed
-    const arrowShape = new ArrowShape( 0, 0, 42, 0, {
-      tailWidth: 12,
-      headWidth: 25,
-      headHeight: 23
-    } );
-    const newChallengeButton = new RectangularPushButton( {
-      baseColor: Color.YELLOW,
-      xMargin: 27,
-      yMargin: 10.9,
-      content: new Path( arrowShape, { fill: 'black' } ),
-      visibleProperty: level.isSolvedProperty,
-      listener: () => this.newChallenge()
-    } );
-    newChallengeButton.centerX = smileyFaceNode.centerX;
-    newChallengeButton.centerY = this.answerButtons.centerY;
-    this.addChild( newChallengeButton );
-
     // create and add startSequenceNode, which is where the text objects in the start sequence are added
     this.textObjectAnimation = null;
     this.startSequenceNode = new Node();
     this.addChild( this.startSequenceNode );
 
     // cancel the animation and hide the startSequenceNode if the startSequencePlayingProperty is set to false
+    // @ts-ignore TODO-TS
     this.level.startSequencePlayingProperty.link( startSequencePlaying => {
       if ( !startSequencePlaying && this.textObjectAnimation ) {
         this.textObjectAnimation.stop();
@@ -187,63 +121,13 @@ class SubitizeGameLevelNode extends NumberPlayGameLevelNode {
     } );
   }
 
-  public reset() {
-    this.pointsAwardedNodeVisibleProperty.reset();
-    this.answerButtons.reset();
-  }
-
-  /**
-   * Sets up a new challenge in the model and in the view.
-   */
-  private newChallenge() {
-    // @ts-ignore TODO-TS
-    this.level.newChallenge();
-    this.pointsAwardedNodeVisibleProperty.value = false;
-    this.answerButtons.reset();
-    if ( NumberPlayQueryParameters.showCorrectAnswer ) {
-      // @ts-ignore TODO-TS
-      this.answerButtons.showCorrectAnswer( this.level.challengeNumberProperty );
-    }
-  }
-
-  /**
-   * Shows or hides a frowny face - if shown, animates it to fade out when the user made an incorrect guess.
-   *
-   * @param {boolean} showFrownyFace
-   */
-  private setFrownyFaceVisibility( showFrownyFace: boolean ) {
-
-    this.frownyFaceNode.visible = showFrownyFace;
-
-    if ( showFrownyFace ) {
-
-      // Animate opacity of frownyFaceNode, fade it out.
-      this.frownyFaceNode.opacityProperty.value = 1;
-      this.frownyFaceAnimation = new Animation( {
-        delay: 1,
-        duration: 0.8,
-        targets: [ {
-          property: this.frownyFaceNode.opacityProperty,
-          easing: Easing.LINEAR,
-          to: 0
-        } ]
-      } );
-
-      this.frownyFaceAnimation.finishEmitter.addListener( () => {
-        this.frownyFaceNode.visible = false;
-        this.frownyFaceAnimation = null;
-      } );
-
-      this.frownyFaceAnimation.start();
-    }
-  }
-
   /**
    * Animates an object in the start sequence to fade out
    * @param {Array} startSequenceText - array of the text that will make the start sequence
    * @param {Vector2} centerPosition
    */
   private setTextObjectVisibility( startSequenceText: string[], centerPosition: Vector2 ) {
+    // @ts-ignore TODO-TS
     this.level.startSequencePlayingProperty.value = true;
 
     // create and add textObject
@@ -275,6 +159,7 @@ class SubitizeGameLevelNode extends NumberPlayGameLevelNode {
         this.setTextObjectVisibility( startSequenceText, textObject.center );
       }
       else {
+        // @ts-ignore TODO-TS
         this.level.startSequencePlayingProperty.reset();
         this.newChallenge();
       }
