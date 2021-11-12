d765e1853a49a6e490715a803a8963a436c283e7
Sam Reid <reids@colorado.edu>
2021-11-11 22:37:40 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

---
+++ js/numberPlayStrings.ts
@@ -0,0 +1,61 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import numberPlay from './numberPlay.js';
+
+type StringsType = {
+  'number-play': {
+    'title': string
+  },
+  'screen': {
+    'ten': string,
+    'twenty': string,
+    'compare': string,
+    'game': string,
+    'lab': string
+  },
+  'zero': string,
+  'one': string,
+  'two': string,
+  'three': string,
+  'four': string,
+  'five': string,
+  'six': string,
+  'seven': string,
+  'eight': string,
+  'nine': string,
+  'ten': string,
+  'eleven': string,
+  'twelve': string,
+  'thirteen': string,
+  'fourteen': string,
+  'fifteen': string,
+  'sixteen': string,
+  'seventeen': string,
+  'eighteen': string,
+  'nineteen': string,
+  'twenty': string,
+  'isLessThan': string,
+  'isMoreThan': string,
+  'isEqualTo': string,
+  'word': string,
+  'total': string,
+  'tenFrame': string,
+  'ones': string,
+  'objects': string,
+  'english': string,
+  'spanish': string,
+  'level': string,
+  'howManyDots': string,
+  'go': string
+};
+
+const numberPlayStrings = getStringModule( 'NUMBER_PLAY' ) as StringsType;
+
+numberPlay.register( 'numberPlayStrings', numberPlayStrings );
+
+export default numberPlayStrings;
