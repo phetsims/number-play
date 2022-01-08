1e08b2c9a455aa59583a3b0151e7de1d6c8fe823
phet-dev <phetadmin@gmail.com>
2022-01-08 03:21:56 -0700

    update copyright dates from daily grunt work

--- js/common/model/OnesPlayArea.ts
+++ js/common/model/OnesPlayArea.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Model for the Explore screen in Make a Ten. Includes the total, cues, and adding in initial numbers. This file was
--- js/common/view/OnesCreatorPanel.ts
+++ js/common/view/OnesCreatorPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * Panel that contains a stack of  paper ones or objects, which can be clicked or dragged to create draggable
