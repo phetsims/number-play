8c31dfbfaffa8ba85a945cbf15608d2a191fb294
phet-dev <phetadmin@gmail.com>
2022-06-23 04:08:10 -0600

    update copyright dates from daily grunt work

--- js/game/view/SubitizeGameLevelNode.ts
+++ js/game/view/SubitizeGameLevelNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * SubitizeGameLevelNode is the class for a 'Subitize' game level view.
