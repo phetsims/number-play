23ff8a1863e40e772fcb10dcde0634c8dbb8c4e3
phet-dev <phetadmin@gmail.com>
2022-07-15 05:26:04 -0600

    update copyright dates from daily grunt work

--- js/game/view/SubitizeLoadingBarNode.ts
+++ js/game/view/SubitizeLoadingBarNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * SubitizeLoadingBarNode contains the loading bar animation that happens after the user presses play when they enter
