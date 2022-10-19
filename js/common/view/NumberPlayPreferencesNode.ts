c0d68116121022c283adb52d1f5aad775e334227
chrisklus <christopher.klusendorf@colorado.edu>
2022-10-18 23:13:33 -0600

    Fix layout of toggle controls, add control descriptions, add translated strings and use string Properties, factor out layout options, see https://github.com/phetsims/number-play/issues/189

--- js/common/view/NumberPlayPreferencesNode.ts
+++ js/common/view/NumberPlayPreferencesNode.ts
@@ -14,28 +14,34 @@ import numberPlayPreferences, { NumberPlayPreferences } from '../model/numberPla
 import HSlider from '../../../../sun/js/HSlider.js';
 import Utils from '../../../../dot/js/Utils.js';
 import Checkbox from '../../../../sun/js/Checkbox.js';
+import NumberPlayStrings from '../../NumberPlayStrings.js';
 
 export default class NumberPlayPreferencesNode extends NumberSuiteCommonPreferencesNode<NumberPlayPreferences> {
 
   public constructor() {
 
-    const gameScreenSectionTitle = new Text( 'Game Screen', {
-      fontWeight: 'bold',
-      fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE
+    const gameLevelsTitle = new Text( NumberPlayStrings.gameLevelsStringProperty,
+      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_BOLD_OPTIONS );
+    const gameLevelsDescription = new Text( NumberPlayStrings.gameLevelsDescriptionStringProperty,
+      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS );
+
+    const gameLevelsHeader = new VBox( {
+      children: [ gameLevelsTitle, gameLevelsDescription ],
+      spacing: NumberSuiteCommonPreferencesNode.CONTROL_DESCRIPTION_SPACING,
+      align: 'left'
     } );
 
     const showCountingLevelOneCheckbox = new Checkbox( numberPlayPreferences.showCountingLevelOneProperty,
-      new Text( 'Counting: Level One', { fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE } ) );
+      new Text( NumberPlayStrings.countingLevelOneStringProperty, NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS ) );
     const showCountingLevelTwoCheckbox = new Checkbox( numberPlayPreferences.showCountingLevelTwoProperty,
-      new Text( 'Counting: Level Two', { fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE } ) );
+      new Text( NumberPlayStrings.countingLevelTwoStringProperty, NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS ) );
     const showSubitizeLevelOneCheckbox = new Checkbox( numberPlayPreferences.showSubitizeLevelOneProperty,
-      new Text( 'Subitize: Level One', { fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE } ) );
+      new Text( NumberPlayStrings.subitizeLevelOneStringProperty, NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS ) );
     const showSubitizeLevelTwoCheckbox = new Checkbox( numberPlayPreferences.showSubitizeLevelTwoProperty,
-      new Text( 'Subitize: Level Two', { fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE } ) );
+      new Text( NumberPlayStrings.subitizeLevelTwoStringProperty, NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS ) );
 
-    const subitizeTimeText = new Text( 'Subitize Time', {
-      fontSize: NumberSuiteCommonPreferencesNode.FONT_SIZE
-    } );
+    const subitizeTimeText = new Text( NumberPlayStrings.subitizeTimeStringProperty,
+      NumberSuiteCommonPreferencesNode.CONTROL_TEXT_BOLD_OPTIONS );
     const subitizeTimeSlider = new HSlider( numberPlayPreferences.subitizeTimeShownProperty,
       numberPlayPreferences.subitizeTimeShownProperty.range!, {
         constrainValue: ( value: number ) => Utils.toFixedNumber( value, 1 )
@@ -43,6 +49,7 @@ export default class NumberPlayPreferencesNode extends NumberSuiteCommonPreferen
     const subitizeTimeControl = new HBox( {
       children: [ subitizeTimeText, subitizeTimeSlider ]
     } );
+
     const gameScreenControls = new VBox( {
       children: [ showCountingLevelOneCheckbox, showCountingLevelTwoCheckbox, showSubitizeLevelOneCheckbox,
         showSubitizeLevelTwoCheckbox, subitizeTimeControl ],
@@ -50,7 +57,7 @@ export default class NumberPlayPreferencesNode extends NumberSuiteCommonPreferen
       align: 'left'
     } );
 
-    super( numberPlayPreferences, [ gameScreenSectionTitle, gameScreenControls ] );
+    super( numberPlayPreferences, [ gameLevelsHeader, gameScreenControls ] );
   }
 }
 
--- js/common/view/NumberSuiteCommonPreferencesNode.ts
+++ js/common/view/NumberSuiteCommonPreferencesNode.ts
@@ -13,12 +13,31 @@ import numberPlay from '../../numberPlay.js';
 import NumberSuiteCommonPreferences from '../model/NumberSuiteCommonPreferences.js';
 import PreferencesToggleSwitch from '../../../../joist/js/preferences/PreferencesToggleSwitch.js';
 import SecondLocaleSelectorCarousel from './SecondLocaleSelectorCarousel.js';
+import NumberPlayStrings from '../../NumberPlayStrings.js';
 
 // constants
 const FONT_SIZE = 16;
+const CONTROL_TEXT_OPTIONS = {
+  fontSize: FONT_SIZE
+};
+const CONTROL_TEXT_BOLD_OPTIONS = {
+  fontSize: FONT_SIZE,
+  fontWeight: 'bold'
+};
+const CONTROL_DESCRIPTION_SPACING = 5;
+const V_BOX_SPACING = 15;
+const V_BOX_OPTIONS: VBoxOptions = {
+  align: 'left',
+  spacing: V_BOX_SPACING
+};
 
 export default abstract class NumberSuiteCommonPreferencesNode<T extends NumberSuiteCommonPreferences> extends HBox {
   public static readonly FONT_SIZE = FONT_SIZE;
+  public static readonly CONTROL_TEXT_OPTIONS = CONTROL_TEXT_OPTIONS;
+  public static readonly CONTROL_TEXT_BOLD_OPTIONS = CONTROL_TEXT_BOLD_OPTIONS;
+  public static readonly CONTROL_DESCRIPTION_SPACING = CONTROL_DESCRIPTION_SPACING;
+  public static readonly V_BOX_SPACING = V_BOX_SPACING;
+  public static readonly V_BOX_OPTIONS = V_BOX_OPTIONS;
 
   protected constructor( preferences: T, additionalControls: Node[] ) {
 
@@ -27,54 +46,42 @@ export default abstract class NumberSuiteCommonPreferencesNode<T extends NumberS
       align: 'top'
     } );
 
-    const vBoxOptions: VBoxOptions = {
-      align: 'left',
-      spacing: 15
-    };
-
-    const generalText = new Text( 'General', {
-      fontSize: FONT_SIZE,
-      fontWeight: 'bold'
-    } );
     const readAloudToggleSwitch = new PreferencesToggleSwitch( preferences.readAloudProperty, false, true, {
-      leftValueLabel: new Text( 'Read Aloud', {
-        fontSize: FONT_SIZE
-      } )
+      labelNode: new Text( NumberPlayStrings.readAloudStringProperty, CONTROL_TEXT_BOLD_OPTIONS ),
+      descriptionNode: new Text( NumberPlayStrings.readAloudDescriptionStringProperty, CONTROL_TEXT_OPTIONS ),
+      descriptionSpacing: CONTROL_DESCRIPTION_SPACING
     } );
     const showSecondLocaleProperty = new PreferencesToggleSwitch( preferences.showSecondLocaleProperty, false, true, {
-      leftValueLabel: new Text( 'Second Language', {
-        fontSize: FONT_SIZE
-      } )
+      labelNode: new Text( NumberPlayStrings.secondLanguageStringProperty, CONTROL_TEXT_BOLD_OPTIONS ),
+      descriptionNode: new Text( NumberPlayStrings.secondLanguageDescriptionStringProperty, CONTROL_TEXT_OPTIONS ),
+      descriptionSpacing: CONTROL_DESCRIPTION_SPACING
     } );
-    const generalControls = new VBox( {
-      children: [ readAloudToggleSwitch, showSecondLocaleProperty ],
-      align: 'left',
-      spacing: 5
-    } );
+    const generalControls = new VBox( combineOptions<VBoxOptions>( {
+      children: [ readAloudToggleSwitch, showSecondLocaleProperty ]
+    }, V_BOX_OPTIONS ) );
 
     const secondLocaleSelectorNode = new SecondLocaleSelectorCarousel();
     preferences.showSecondLocaleProperty.link( showSecondLocale => {
       secondLocaleSelectorNode.visible = showSecondLocale;
     } );
 
-    const leftVBox = new VBox( combineOptions<VBoxOptions>( vBoxOptions, {
-      excludeInvisibleChildrenFromBounds: false
-    } ) );
-    leftVBox.children = [ generalText, generalControls, secondLocaleSelectorNode ];
-
-    const labScreenText = new Text( 'Lab Screen', {
-      fontWeight: 'bold',
-      fontSize: FONT_SIZE
+    const leftControls = new VBox( {
+      children: [ generalControls, secondLocaleSelectorNode ],
+      excludeInvisibleChildrenFromBounds: false,
+      align: 'center',
+      spacing: V_BOX_SPACING
     } );
+
     const showLabOnesToggleSwitch = new PreferencesToggleSwitch( preferences.showLabOnesProperty, false, true, {
-      leftValueLabel: new Text( 'Show Ones', {
-        fontSize: FONT_SIZE
-      } )
+      labelNode: new Text( NumberPlayStrings.showOnesStringProperty, CONTROL_TEXT_BOLD_OPTIONS ),
+      descriptionNode: new Text( NumberPlayStrings.showOnesDescriptionStringProperty, CONTROL_TEXT_OPTIONS ),
+      descriptionSpacing: CONTROL_DESCRIPTION_SPACING
     } );
-    const rightVBox = new VBox( vBoxOptions );
-    rightVBox.children = [ ...additionalControls, labScreenText, showLabOnesToggleSwitch ];
+    const rightControls = new VBox( combineOptions<VBoxOptions>( {
+      children: [ ...additionalControls, showLabOnesToggleSwitch ]
+    }, V_BOX_OPTIONS ) );
 
-    this.children = [ leftVBox, rightVBox ];
+    this.children = [ leftControls, rightControls ];
   }
 }
 
