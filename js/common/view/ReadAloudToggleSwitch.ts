a625d48e0bf4b41cc2249aec8faa86e6fc206844
chrisklus <christopher.klusendorf@colorado.edu>
2022-11-14 12:37:22 -0700

    Add spacer to readAloud toggle, see https://github.com/phetsims/number-play/issues/189

--- js/common/view/ReadAloudToggleSwitch.ts
+++ js/common/view/ReadAloudToggleSwitch.ts
@@ -8,7 +8,7 @@
  * @author Marla Schulz (PhET Interactive Simulations)
  */
 
-import { Text } from '../../../../scenery/js/imports.js';
+import { Node, Rectangle, Text } from '../../../../scenery/js/imports.js';
 import numberPlay from '../../numberPlay.js';
 import NumberSuiteCommonPreferences from '../model/NumberSuiteCommonPreferences.js';
 import PreferencesToggleSwitch from '../../../../joist/js/preferences/PreferencesToggleSwitch.js';
@@ -16,20 +16,30 @@ import NumberPlayStrings from '../../NumberPlayStrings.js';
 import NumberSuiteCommonPreferencesNode from './NumberSuiteCommonPreferencesNode.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 
-export default class ReadAloudToggleSwitch<T extends NumberSuiteCommonPreferences> extends PreferencesToggleSwitch<boolean> {
+export default class ReadAloudToggleSwitch<T extends NumberSuiteCommonPreferences> extends Node {
 
   public constructor( preferences: T, screens: number[], labelProperty: TReadOnlyProperty<string> ) {
 
-    super( preferences.readAloudProperty, false, true, {
+    super();
+
+    const toggleSwitch = new PreferencesToggleSwitch( preferences.readAloudProperty, false, true, {
       labelNode: new Text( labelProperty, NumberSuiteCommonPreferencesNode.CONTROL_TEXT_BOLD_OPTIONS ),
       descriptionNode: new Text( NumberPlayStrings.readAloudDescriptionStringProperty,
         NumberSuiteCommonPreferencesNode.CONTROL_TEXT_OPTIONS ),
       descriptionSpacing: NumberSuiteCommonPreferencesNode.CONTROL_DESCRIPTION_SPACING
     } );
+    this.addChild( toggleSwitch );
+
+    // TODO: Remove this space and convert this class back to a PreferencesToggleSwitch once spacing in the audio tab
+    // is supported or the sounds
+    const spacerSize = 40;
+    const spacer = new Rectangle( 0, 0, spacerSize, spacerSize );
+    spacer.left = toggleSwitch.right;
+    this.addChild( spacer );
 
     if ( QueryStringMachine.containsKey( 'screens' ) ) {
       const includedScreens = phet.chipper.queryParameters.screens;
-      this.enabled = _.some( includedScreens, includedScreen => screens.includes( includedScreen ) );
+      toggleSwitch.enabled = _.some( includedScreens, includedScreen => screens.includes( includedScreen ) );
     }
   }
 }
