a1f0f39ed7f2e949f20b865cf040a3cafb547901
phet-dev <phetadmin@gmail.com>
2023-01-12 04:54:24 -0700

    update copyright dates from daily grunt work

--- js/game/view/NumberPlayGameLevelSelectionNode.ts
+++ js/game/view/NumberPlayGameLevelSelectionNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * NumberPlayGameLevelSelectionNode is the user interface for level selection and other game settings in the 'Game'
