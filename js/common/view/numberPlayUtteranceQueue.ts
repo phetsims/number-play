c7f3685172aee54703598dc427b7ee4e6f6bed9c
phet-dev <phetadmin@gmail.com>
2023-02-16 04:41:39 -0700

    update copyright dates from daily grunt work

--- js/common/view/numberPlayUtteranceQueue.ts
+++ js/common/view/numberPlayUtteranceQueue.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * A singleton UtteranceQueue that is used for voicing specific to Number Play. This is needed because Number Play
