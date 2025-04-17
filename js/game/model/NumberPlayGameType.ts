0d5eb5f50dd487b2f28cbd50df4708b3da719d2c
phet-dev <phetadmin@gmail.com>
2025-04-17 04:05:49 -0600

    update copyright dates from daily grunt work

--- js/game/model/NumberPlayGameType.ts
+++ js/game/model/NumberPlayGameType.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  *  NumberPlayGameType identifies the game type in Number Play.
